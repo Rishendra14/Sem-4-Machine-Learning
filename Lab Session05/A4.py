cf7d8f81e36962513dda6a18d98eb490c6f5642f
Rishendra14 <rishendrayallanki@gmail.com>
2025-02-24 23:07:49 +0530

    Update A4.py

--- Lab Session05/A4.py
+++ Lab Session05/A4.py
@@ -3,7 +3,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.cluster import KMeans
 
-# Load and merge Excel files (adjust file paths as needed)
 all_data = []
 file_paths = ['255_s.xlsx', '259_s.xlsx', '261_s.xlsx', '285_s.xlsx', '287_s.xlsx',
               '219_student.xlsx', '220_student.xlsx', '221_student.xlsx', '222_student.xlsx', '223_student.xlsx']
@@ -14,19 +13,19 @@ for file in file_paths:
 df_combined = pd.concat(all_data, ignore_index=True)
 df_combined.columns = df_combined.columns.str.strip()
 
-# Create "Label" column
+
 if "Label" not in df_combined.columns:
     df_combined["Label"] = df_combined["Member"].apply(lambda x: 1 if x == "Student" else 0)
 
-# Select numerical columns
+
 numerical_columns = ["Start time", "End time"]
 X = df_combined[numerical_columns].values
 y = df_combined["Label"].values
 
-# Split data
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Perform K-Means clustering
+
 kmeans = KMeans(n_clusters=2, random_state=42, n_init="auto").fit(X_train)
 
 # Output K-Means results
